0c536ad7a560d007e54cf60d55b2a3d1b9c44a2c
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-04-20 08:27:24 -0300

    style: ajustado responsividade do card com tv em HomeAbout

--- resources/js/Components/HomeAbout.jsx
+++ resources/js/Components/HomeAbout.jsx
@@ -48,23 +48,23 @@ const HomeAbout = () => {
                     ))}
                 </div>
 
-                <div className="bg-secondary py-3 grid grid-cols-2 items-center">
-                    <div className="flex items-center">
-                        <img src={tvAward} alt="Smart TV Samsung 50”" className="-my-[5.5%] -ml-[19%] -mr-[10%] w-[524px]" />
+                <div className="bg-secondary py-3 grid md:grid-cols-2 items-center">
+                    <div className="flex flex-col md:flex-row items-center">
+                        <img src={tvAward} alt="Smart TV Samsung 50”" className="m-auto -my-[5.5%] md:-ml-[19%] md:-mr-[10%] w-[524px]" />
 
                         <div>
-                            <h3 className="font-secondary text-primary text-5xl 2xl:text-[56px] font-semibold uppercase tracking-tight mb-3">Smart TV<br /> Samsung 50”</h3>
+                            <h3 className="font-secondary text-primary text-4xl lg:text-5xl 2xl:text-[56px] font-semibold uppercase tracking-tight mb-3">Smart TV<br /> Samsung 50”</h3>
 
-                            <p className="tracking-tight text-[17px]">Viva a Copa do jeito que o Brasil merece</p>
+                            <p className="tracking-tight text-sm lg:text-[17px]">Viva a Copa do jeito que o Brasil merece</p>
                         </div>
                     </div>
 
                     <div className="flex items-center gap-6">
-                        <div>
-                            <p className="text-lg font-semibold tracking-tight">Projetos acima de</p>
-                            <h3 className="font-secondary text-primary text-6xl 2xl:text-[74px] font-semibold uppercase tracking-tight">R$ 49.990</h3>
+                        <div className='ml-16 mt-6 md:ml-20 md:mt-0 xl:ml-0'>
+                            <p className="text-base lg:text-lg font-semibold tracking-tight">Projetos acima de</p>
+                            <h3 className="font-secondary text-primary text-5xl lg:text-6xl 2xl:text-[74px] font-semibold uppercase tracking-tight">R$ 49.990</h3>
                             
-                            <p className="text-lg font-semibold tracking-tight">já garantem seu prêmio</p>
+                            <p className="text-base lg:text-lg font-semibold tracking-tight">já garantem seu prêmio</p>
                         </div>
                     </div>
                 </div>
