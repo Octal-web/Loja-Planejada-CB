3c3334676f5115562d616e0161fb64b12a2825e3
RafaLaste <rafinhalaste@hotmail.com>
2026-04-22 14:49:27 -0300

    refactor: ajuste build deploy

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: '/jogada-planejada/',
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
             refresh: true,
-            buildDirectory: 'jogada-planejada',
         }),
         react(),
     ],
