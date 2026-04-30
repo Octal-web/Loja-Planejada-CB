389e22935029938fb0eee088fef149835937ecaa
RafaLaste <rafinhalaste@hotmail.com>
2026-04-30 09:31:53 -0300

    refactor: correção data da promoção

--- resources/js/Layouts/DefaultLayout.jsx
+++ resources/js/Layouts/DefaultLayout.jsx
@@ -262,7 +262,7 @@ const DefaultLayout = ({ children }) => {
                             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.
                         </p>
 
-                        <p className="text-sm text-white text-center font-bold opacity-70 pt-4">Promoção valida até: xx/xx/xxxx</p>
+                        <p className="text-sm text-white text-center font-bold opacity-70 pt-4">Promoção válida de 05/05/2026 até: 17/07/2026</p>
 
                         <ul className="flex justify-center gap-3 mt-8">
                             <li>
